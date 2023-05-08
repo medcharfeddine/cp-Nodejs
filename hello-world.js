af4d84da730dfeba1782258525a42fa233f0b077
medcharfeddine <medcharfeddine@outlook.com>
2023-05-08 15:01:36 +0100

    cp-nodejs

---
+++ hello-world.js
@@ -0,0 +1,32 @@
+//First App
+console.log("Hello World");
+
+//Second App
+
+var http = require("http");
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+    response.end("<h1>Hello Node!!!!</h1>\n");
+  })
+  .listen(3000);
+
+console.log("Server running at http://127.0.0.1:8081/");
+
+//Third App
+
+var fs = require("fs");
+
+fs.appendFile("welcome.txt", "Hello Node", function (err) {
+  if (err) throw err;
+  console.log("Saved!");
+});
+
+fs.readFile("hello.txt", function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Asynchronous read: " + data.toString());
+});
